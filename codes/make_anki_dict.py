803d7f6169d15ddd9031fe3cf9b9f8b464aa38c2
h4ziel <hazielhayan6@gmail.com>
2024-05-13 23:21:33 -0300

    miss

--- codes/make_anki_dict.py
+++ codes/make_anki_dict.py
@@ -3,7 +3,7 @@ context:
 
 i was looking for a good japanese anki deck to practice and came
 across this post on reddit, i decided to download the files as html and use
-the requests API to take only the word and it's meaning.
+the beautiful soup API to take only the word and it's meaning.
 
 https://www.reddit.com/r/LearnJapanese/comments/s2iop/heres_a_spreadsheet_of_the_6000_most_common/
 
@@ -14,6 +14,9 @@ and the second being the txt file to store the words, this script will
 simply store the results as "言葉, [ことば] word" for each word.
 """
 
+
+
+# get next word in the 
 def get_next_word():
     pass
 
