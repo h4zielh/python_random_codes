7ed9d6f1a18f389eaf7d66fdf01bfa0609b8d74c
haziel <167656882+h4zielh@users.noreply.github.com>
2024-05-13 23:01:39 -0300

    added context

---
+++ codes/make_anki_dict.py
@@ -0,0 +1,16 @@
+"""
+context:
+
+i was looking for a good japanese anki deck to practice and came
+across this post on reddit, i decided to download the files as html and use
+the requests API to take only the word and it's meaning.
+
+https://www.reddit.com/r/LearnJapanese/comments/s2iop/heres_a_spreadsheet_of_the_6000_most_common/
+
+how to use this script:
+
+execute the script giving it two arguments, the first being the html file
+and the second being the txt file to store the words, this script will
+simply store the results as "言葉, [ことば] word" for each word.
+"""
+
